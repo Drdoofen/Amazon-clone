3276069b37be66ffe0da5381c6e2db4a8dda1897
Golden Ret <stanprasad9999@gmail.com>
2023-09-08 17:50:57 +0530

    working navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCSUJbWvI7ZwMZ87xEfbVDKzQ4FLS_gobs",
+    authDomain: "challenge-fae21.firebaseapp.com",
+    projectId: "challenge-fae21",
+    storageBucket: "challenge-fae21.appspot.com",
+    messagingSenderId: "972339380988",
+    appId: "1:972339380988:web:5ef75e87f1adcd5a9e8d79",
+    measurementId: "G-T1SGSMZPQN"
+  };
