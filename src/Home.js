5c90097727f9aeda4a82fc32b3f2b7d3c1387d44
Golden Ret <stanprasad9999@gmail.com>
2023-09-09 22:10:00 +0530

    added props

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,13 @@ function Home() {
             />
 
             <div className='home__row'>
-              <Product/>
+              <Product 
+              title='The lean startup' 
+              price={30.99}
+              image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+              rating={5}
+
+              />
               <Product/>
             </div>
             <div className='home__row'>
