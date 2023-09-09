9eb0a82635b59b58ec7224e4592b39647d86ce19
Golden Ret <stanprasad9999@gmail.com>
2023-09-09 21:49:05 +0530

    improved

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
 
 function Header() {
   return (
-    <div className='header'>Header
+    <div className='header'>
     
     <img 
     className="header__logo"
@@ -47,6 +49,12 @@ function Header() {
             <span className="header__optionLineTwo">
                 Prime
             </span>
+        </div>
+
+        <div className='header__optionBasket'>
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo
+            header__basketCount">0</span>
         </div>
     
     </div>
