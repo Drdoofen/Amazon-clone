dbfe758b7f5250a39333273c33e570664863d03a
Golden Ret <stanprasad9999@gmail.com>
2023-09-10 15:35:36 +0530

    added paths, newly react router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import './App.css';
 import React from 'react';
 import Header from './Header.js';
 import Home from './Home';
-import {BrowserRouter as Router, Switch, Route}
+import {BrowserRouter as Router, Routes, Route}
 from "react-router-dom"
 function App() {
   return (
     <Router>
       <div className="app">
-        <Switch>
-          <Route path="/">
-            <Header/>
-            <Home/>
-          </Route>
-        </Switch>
+        <Routes>
+        <Route path="/" element={ <div><Header/><Home/> </div>} />
+        <Route path="/checkout" element={<h1>Hello checkout</h1>} />
+        </Routes>
       </div>
     </Router>
-    
   );
 }
 
