48ffb0e115a200ebe284a0100af6e21a9d895f22
Golden Ret <stanprasad9999@gmail.com>
2023-09-24 22:43:31 +0530

    add

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import "./Product.css";
-function Product({title, image, price, rating}) {
+import { useStateValue } from "./StateProvider.js";
+function Product({id, title, image, price, rating}) {
+
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET', 
+            item: {
+                id: id, 
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
+
   return (
     <div className='product'>
         <div className='product__info'>
@@ -22,7 +39,7 @@ function Product({title, image, price, rating}) {
             src={image}
             alt=""
         />
-        <button>Add to Basket</button> 
+        <button onClick={addToBasket}>Add to Basket</button> 
     </div>
   )
 }
